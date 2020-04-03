04152311686e9a99a64e07f7336f4c5f4c862dd3
Siva Karuppiah <sivakg2000@gmail.com>
2020-04-03 15:35:31 +0530

    counter component added.

--- examples/src/app/app-routing.module.ts
+++ examples/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TestComponent } from "./test/test.component";
+import { CounterComponent } from "./counter/counter.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "test", component: TestComponent },
+  { path: "counter", component: CounterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- examples/src/app/app.module.ts
+++ examples/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { CpCounterComponent } from "./cp-counter/cp-counter.component";
+import { TestComponent } from "./test/test.component";
+import { CounterComponent } from "./counter/counter.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CpCounterComponent,
+    TestComponent,
+    CounterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
